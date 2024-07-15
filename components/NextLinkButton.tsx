1a51ae47ff4114d311239ecf2babeab23594393d
tomoru04 <shodasandesuyo@icloud.com>
2024-07-15 20:43:45 +0900

    modify: gas

--- components/NextLinkButton.tsx
+++
@@ -1,7 +0,0 @@
-const NextLinkButton = (props: ComponentProps<typeof Button>) => {
-  const { isDisabled } = props;
-  if (isDisabled) {
-    return <Button as="a" {...props} href={undefined} />;
-  }
-  return <Button as={NextLink} {...props} />;
-};
